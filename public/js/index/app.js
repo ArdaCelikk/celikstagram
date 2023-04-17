3b4ac9baf4ec7c23a532356907321db697f12b05
Arda Çelik <ardac_555@hotmail.com>
2023-04-17 10:52:06 +0300

    Like Bug Fix

--- public/js/index/app.js
+++ public/js/index/app.js
@@ -1,29 +1,5 @@
 
-const likePost = async ()=>{
-    let frontEndLikeCount = Number(this.parentElement.children[1].innerHTML)
-    const res = await fetch("/posts/like", {
-    method: "POST", // or 'PUT'
-    headers: {
-        "Content-Type": "application/json",
-    },
-    body: JSON.stringify({
-        postID:this.parentElement.lastElementChild.innerHTML
-    }),
-    })
-    const response= await  res.json();
-    if(response.succeded){
-        
-        if(response.like){
-            this.parentElement.children[1].textContent = frontEndLikeCount+1;
-            this.style.color = "crimson"
-            
-            console.log(frontEndLikeCount);
-        }else{
-            this.style.color = "white"
-            this.parentElement.children[1].textContent = frontEndLikeCount-1
-        }
-    }
-}
+
 
 
 let like = document.getElementsByClassName("btnLike")
@@ -31,6 +7,28 @@ var j;
 for(j=0; j< like.length;j++)
     {
         like[j].onclick = async function(){
-            likePost()
+            let frontEndLikeCount = Number(this.parentElement.children[1].innerHTML)
+            const res = await fetch("/posts/like", {
+            method: "POST", // or 'PUT'
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                postID:this.parentElement.lastElementChild.innerHTML
+            }),
+            })
+            const response= await  res.json();
+            if(response.succeded){
+                
+                if(response.like){
+                    this.parentElement.children[1].textContent = frontEndLikeCount+1;
+                    this.style.color = "crimson"
+                    
+                    console.log(frontEndLikeCount);
+                }else{
+                    this.style.color = "white"
+                    this.parentElement.children[1].textContent = frontEndLikeCount-1
+                }
+            }
         }
     }
